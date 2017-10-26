fb11a5be1c1592d816e746b8be2119cb7535b981
Menghua Xiao <menxiao@microsoft.com>
2017-10-26 16:52:42 +0800

    Working with ACSRule

--- src/test/java/com/microsoft/jenkins/acs/integration/IntegrationTest.java
+++ src/test/java/com/microsoft/jenkins/acs/integration/IntegrationTest.java
@@ -6,42 +6,25 @@
 
 package com.microsoft.jenkins.acs.integration;
 
-import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.microsoft.azure.CloudException;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.util.AzureCredentials;
-import com.microsoft.jenkins.acs.util.AzureHelper;
 import com.microsoft.jenkins.kubernetes.credentials.ResolvedDockerRegistryEndpoint;
-import hudson.FilePath;
 import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.docker.commons.credentials.DockerRegistryToken;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static com.microsoft.jenkins.acs.integration.TestHelpers.loadProperty;
 
 public abstract class IntegrationTest {
     @ClassRule
@@ -59,107 +42,32 @@ public abstract class IntegrationTest {
     private static final Logger LOGGER = Logger.getLogger(IntegrationTest.class.getName());
 
     protected TestEnvironment testEnv = null;
-    protected AzureCredentials.ServicePrincipal servicePrincipal = null;
-    protected Azure azure = null;
-    protected KeyPair keyPair = null;
-    protected SSHUserPrivateKey sshCredentials = null;
 
     @Before
     public void setup() throws Exception {
         testEnv = new TestEnvironment();
-        LOGGER.log(Level.INFO, "Setting up test environment for {0}, resource group {1}, name {2}",
-                new Object[]{testEnv.containerServiceType, testEnv.resourceGroup, testEnv.resourceName});
-        servicePrincipal = new AzureCredentials.ServicePrincipal(
-                testEnv.subscriptionId,
-                testEnv.clientId,
-                testEnv.clientSecret,
-                testEnv.oauth2TokenEndpoint,
-                testEnv.serviceManagementURL,
-                testEnv.authenticationEndpoint,
-                testEnv.resourceManagerEndpoint,
-                testEnv.graphEndpoint);
-        azure = AzureHelper.buildClientFromServicePrincipal(servicePrincipal);
-        keyPair = new KeyPair();
-
-        LOGGER.log(Level.INFO, "SSH Key used for test container service creation: \npublic key:\n{0}\nprivate key:\n{1}",
-                new Object[]{keyPair.publicKey, keyPair.privateKey});
-
-        sshCredentials = mock(SSHUserPrivateKey.class);
-        when(sshCredentials.getPrivateKey()).thenReturn(keyPair.privateKey);
-        when(sshCredentials.getPrivateKeys()).thenReturn(Collections.singletonList(keyPair.privateKey));
-    }
-
-    @After
-    public void teardown() throws Exception {
-        clearAzureResources();
-    }
-
-    protected void clearAzureResources() {
-        try {
-            azure.resourceGroups().deleteByName(testEnv.resourceGroup);
-        } catch (CloudException e) {
-            if (e.response().code() != 404) {
-                LOGGER.log(Level.SEVERE, null, e);
-            }
-        }
-    }
-
-    public static String generateRandomString(int length) {
-        String uuid = UUID.randomUUID().toString();
-        return uuid.replaceAll("[^a-z0-9]", "a").substring(0, length);
-    }
-
-    public static void loadFile(Class<?> clazz, FilePath workspace, String sourcePath, String destinationPath) throws Exception {
-        FilePath dest = new FilePath(workspace, destinationPath);
-        dest.getParent().mkdirs();
-        try (InputStream in = clazz.getResourceAsStream(sourcePath);
-             OutputStream out = dest.write()) {
-            IOUtils.copy(in, out);
-        }
     }
 
     protected static class TestEnvironment {
-        public final String subscriptionId;
-        public final String clientId;
-        public final String clientSecret;
-        public final String oauth2TokenEndpoint;
-        public final String serviceManagementURL;
-        public final String authenticationEndpoint;
-        public final String resourceManagerEndpoint;
-        public final String graphEndpoint;
-
-        public final String location;
-        public final String resourceGroup;
-        public final String resourceName;
-        public final String containerServiceType;
-
-        public final String adminUser = "azureuser";
-
+        public final String dockerRegistry;
         public final String dockerUsername;
         public final String dockerPassword;
-        public final String dockerRegistry;
+        public final String dockerRepository;
 
         public final List<ResolvedDockerRegistryEndpoint> dockerCredentials;
 
         protected TestEnvironment() throws MalformedURLException {
-            subscriptionId = loadProperty("ACS_TEST_SUBSCRIPTION_ID");
-            clientId = loadProperty("ACS_TEST_CLIENT_ID");
-            clientSecret = loadProperty("ACS_TEST_CLIENT_SECRET");
-            oauth2TokenEndpoint = "https://login.windows.net/" + loadProperty("ACS_TEST_TENANT");
-
-            serviceManagementURL = loadProperty("ACS_TEST_AZURE_MANAGEMENT_URL", "https://management.core.windows.net/");
-            authenticationEndpoint = loadProperty("ACS_TEST_AZURE_AUTH_URL", "https://login.microsoftonline.com/");
-            resourceManagerEndpoint = loadProperty("ACS_TEST_AZURE_RESOURCE_URL", "https://management.azure.com/");
-            graphEndpoint = loadProperty("ACS_TEST_AZURE_GRAPH_URL", "https://graph.windows.net/");
-
-            location = loadProperty("ACS_TEST_LOCATION", "SoutheastAsia");
-            resourceGroup = loadProperty("ACS_TEST_RESOURCE_GROUP_PREFIX", "acs-test-" + generateRandomString(10));
-            resourceName = loadProperty("ACS_TEST_RESOURCE_NAME_PREFIX", "acs-test-" + generateRandomString(10));
-            containerServiceType = loadProperty("ACS_TEST_CONTAINER_TYPE", "Kubernetes");
-
+            // this will be used for the private docker image pulling test
+            // apart from the settings here, we need to prepare an image on the repository first
+            // RUN:
+            //     docker login $ACS_TEST_DOCKER_REGISTRY
+            //     docker pull nginx
+            //     docker tag nginx:latest $ACS_TEST_DOCKER_REPOSITORY/acs-test-nginx
+            //     docker push $ACS_TEST_DOCKER_REPOSITORY/acs-test-nginx
+            dockerRegistry = loadProperty("ACS_TEST_DOCKER_REGISTRY", "");
             dockerUsername = loadProperty("ACS_TEST_DOCKER_USERNAME", "");
             dockerPassword = loadProperty("ACS_TEST_DOCKER_PASSWORD", "");
-            dockerRegistry = loadProperty("ACS_TEST_DOCKER_REGISTRY", "");
+            dockerRepository = loadProperty("ACS_TEST_DOCKER_REPOSITORY", "");
 
             dockerCredentials = new ArrayList<>();
             if (StringUtils.isNotBlank(dockerUsername) && StringUtils.isNotEmpty(dockerPassword)) {
@@ -168,41 +76,6 @@ public abstract class IntegrationTest {
                 String registry = StringUtils.isBlank(dockerRegistry) ? "https://index.docker.io/v1/" : dockerRegistry;
                 dockerCredentials.add(new ResolvedDockerRegistryEndpoint(new URL(registry), token));
             }
-        }
-
-        private static String loadProperty(final String name) {
-            return loadProperty(name, "");
-        }
-
-        private static String loadProperty(final String name, final String defaultValue) {
-            final String value = System.getProperty(name);
-            if (StringUtils.isEmpty(value)) {
-                return loadEnv(name, defaultValue);
-            }
-            return value;
-        }
-
-        private static String loadEnv(final String name, final String defaultValue) {
-            String value = System.getenv(name);
-            if (StringUtils.isEmpty(value)) {
-                return defaultValue;
-            }
-            return value;
-        }
-    }
-
-    protected static class KeyPair {
-        public String publicKey;
-        public String privateKey;
-
-        public KeyPair() throws JSchException {
-            com.jcraft.jsch.KeyPair kpair = com.jcraft.jsch.KeyPair.genKeyPair(new JSch(), com.jcraft.jsch.KeyPair.RSA);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            kpair.writePrivateKey(out);
-            privateKey = out.toString();
-            out.reset();
-            kpair.writePublicKey(out, "acs-test");
-            publicKey = out.toString();
         }
     }
 }
--- src/test/java/com/microsoft/jenkins/acs/integration/KubernetesDeploymentIT.java
+++ src/test/java/com/microsoft/jenkins/acs/integration/KubernetesDeploymentIT.java
@@ -6,29 +6,34 @@
 
 package com.microsoft.jenkins.acs.integration;
 
-import com.google.common.io.Files;
-import com.microsoft.azure.management.compute.ContainerService;
-import com.microsoft.azure.management.compute.ContainerServiceMasterProfileCount;
 import com.microsoft.azure.management.compute.ContainerServiceOchestratorTypes;
-import com.microsoft.azure.management.compute.ContainerServiceVMSizeTypes;
-import com.microsoft.azure.management.resources.ResourceGroup;
 import com.microsoft.jenkins.acs.commands.KubernetesDeploymentCommand;
 import com.microsoft.jenkins.acs.commands.KubernetesDeploymentCommandBase;
+import com.microsoft.jenkins.acs.util.Constants;
 import com.microsoft.jenkins.azurecommons.JobContext;
 import com.microsoft.jenkins.azurecommons.command.CommandState;
+import com.microsoft.jenkins.azurecommons.remote.SSHClient;
+import com.microsoft.jenkins.kubernetes.KubernetesClientWrapper;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import org.junit.After;
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.util.logging.Logger;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
+import static com.microsoft.jenkins.acs.integration.TestHelpers.generateRandomString;
+import static com.microsoft.jenkins.acs.integration.TestHelpers.loadFile;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -36,9 +41,18 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KubernetesDeploymentIT extends IntegrationTest {
-    private static final Logger LOGGER = Logger.getLogger(KubernetesDeploymentIT.class.getName());
+    /**
+     * The rule to create the resource group and ACS cluster before the test and delete them afterwards.
+     * <p>
+     * It takes about 10 minutes for the ACS provision. So we would better provision the ACS for each test suite
+     * using {@link ClassRule} rather than each test method ({@link org.junit.Rule}).
+     */
+    @ClassRule
+    public static ACSRule acs = new ACSRule(ContainerServiceOchestratorTypes.KUBERNETES.toString());
 
-    private ContainerService containerService = null;
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
+
     private FilePath workspace = null;
     private KubernetesDeploymentCommandBase.IKubernetesDeploymentCommandData commandData;
     private EnvVars envVars = new EnvVars();
@@ -50,77 +64,84 @@ public class KubernetesDeploymentIT extends IntegrationTest {
     public void setup() throws Exception {
         super.setup();
 
-        ResourceGroup resourceGroup = azure.resourceGroups()
-                .define(testEnv.resourceGroup)
-                .withRegion(testEnv.location)
-                .create();
-        Assert.assertNotNull(resourceGroup);
-
-        containerService = azure.containerServices()
-                .define(testEnv.resourceName)
-                .withRegion(testEnv.location)
-                .withExistingResourceGroup(testEnv.resourceGroup)
-                .withKubernetesOrchestration()
-                .withServicePrincipal(servicePrincipal.getClientId(), servicePrincipal.getClientSecret())
-                .withLinux()
-                .withRootUsername(testEnv.adminUser)
-                .withSshKey(keyPair.publicKey)
-                .withMasterNodeCount(ContainerServiceMasterProfileCount.MIN)
-                .withMasterLeafDomainLabel(testEnv.resourceName)
-                .defineAgentPool("pool")
-                .withVMCount(1)
-                .withVMSize(ContainerServiceVMSizeTypes.STANDARD_A1)
-                .withLeafDomainLabel(testEnv.resourceName + "agent")
-                .attach()
-                .create();
-        Assert.assertNotNull(containerService);
-
-        File workspaceDir = Files.createTempDir();
-        workspaceDir.deleteOnExit();
-        workspace = new FilePath(workspaceDir);
+        workspace = new FilePath(tmpFolder.newFolder());
 
         commandData = mock(KubernetesDeploymentCommandBase.IKubernetesDeploymentCommandData.class);
 
         JobContext jobContext = mock(JobContext.class);
         when(jobContext.getWorkspace()).thenReturn(workspace);
         TaskListener taskListener = mock(TaskListener.class);
-        when(taskListener.getLogger()).thenReturn(System.out);
+        when(taskListener.getLogger()).thenReturn(System.err);
         when(jobContext.getTaskListener()).thenReturn(taskListener);
         Run run = mock(Run.class);
-        when(run.getDisplayName()).thenReturn(testEnv.resourceName + "-run");
+        when(run.getDisplayName()).thenReturn(acs.resourceName + "-run");
         when(run.getParent()).thenReturn(mock(Job.class));
         when(jobContext.getRun()).thenReturn(run);
         when(commandData.getJobContext()).thenReturn(jobContext);
 
         when(commandData.getEnvVars()).thenReturn(envVars);
-        when(commandData.getMgmtFQDN()).thenReturn(containerService.masterFqdn());
+        when(commandData.getMgmtFQDN()).thenReturn(acs.containerService.masterFqdn());
         when(commandData.getContainerServiceType()).thenReturn(ContainerServiceOchestratorTypes.KUBERNETES.toString());
-        when(commandData.getSshCredentials()).thenReturn(sshCredentials);
+        when(commandData.getSshCredentials()).thenReturn(acs.sshCredentials);
         when(commandData.getSecretName()).thenReturn(secretName = "acs-test-" + generateRandomString(10));
         when(commandData.getSecretNamespace()).thenReturn("default");
-        when(commandData.isEnableConfigSubstitution()).thenReturn(true);
-        when(commandData.getConfigFilePaths()).thenReturn("k8s.yml");
+        when(commandData.isEnableConfigSubstitution()).thenReturn(false);
+        when(commandData.getConfigFilePaths()).thenReturn("k8s/*.yml");
         when(commandData.getOrchestratorType()).thenReturn(ContainerServiceOchestratorTypes.KUBERNETES);
 
         when(commandData.resolvedDockerRegistryEndpoints(any(Job.class))).thenReturn(testEnv.dockerCredentials);
     }
 
-    @After
-    @Override
-    public void teardown() throws Exception {
-        super.teardown();
-        if (workspace != null) {
-            workspace.deleteRecursive();
-        }
-    }
-
     @Test
-    public void kubernetesDeployment() throws Exception {
-        loadFile(KubernetesDeploymentIT.class, workspace, "k8s.yml", "k8s.yml");
+    public void combinedResourceDeployment() throws Exception {
+        loadFile(KubernetesDeploymentIT.class, workspace, "k8s/combined-resource.yml");
 
         new KubernetesDeploymentCommand().execute(commandData);
 
         verify(commandData).setCommandState(CommandState.Success);
         verify(commandData, never()).logError(any(Exception.class));
+    }
+
+    @Test
+    public void separeteResourceDeployment() throws Exception {
+        loadFile(KubernetesDeploymentIT.class, workspace, "k8s/separated-deployment.yml");
+        loadFile(KubernetesDeploymentIT.class, workspace, "k8s/separated-service.yml");
+
+        new KubernetesDeploymentCommand().execute(commandData);
+        verify(commandData).setCommandState(CommandState.Success);
+        verify(commandData, never()).logError(any(Exception.class));
+    }
+
+    @Test
+    public void testVariableSubstitute() throws Exception {
+        when(commandData.isEnableConfigSubstitution()).thenReturn(true);
+        envVars.put("DEPLOYMENT_NAME", "substitute-vars-deployment");
+        envVars.put("LABEL_APP", "substitute-nginx");
+
+        loadFile(KubernetesDeploymentIT.class, workspace, "k8s/substitute.yml");
+
+        new KubernetesDeploymentCommand().execute(commandData);
+        verify(commandData).setCommandState(CommandState.Success);
+        verify(commandData, never()).logError(any(Exception.class));
+
+        KubernetesClient client = buildKubernetesClient();
+        Deployment deployment = client.extensions().deployments().inNamespace("default").withName("substitute-vars-deployment").get();
+        Assert.assertNotNull(deployment);
+        Assert.assertEquals("substitute-nginx", deployment.getMetadata().getLabels().get("app"));
+    }
+
+    @Test
+    public void testDockerCredentials() throws Exception {
+
+    }
+
+    private KubernetesClient buildKubernetesClient() throws Exception {
+        File config = tmpFolder.newFile();
+        SSHClient client = new SSHClient(acs.containerService.masterFqdn(), Constants.KUBERNETES_SSH_PORT, acs.adminUser, null, acs.keyPair.privateKey);
+        try (SSHClient connected = client.connect();
+             OutputStream os = new FileOutputStream(config)) {
+            connected.copyFrom(".kube/config", os);
+        }
+        return new KubernetesClientWrapper(config.getAbsolutePath()).getClient();
     }
 }
